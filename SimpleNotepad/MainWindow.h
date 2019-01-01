fee4ad0e98395fdcbb76dd13c2d1764db5b9145f
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-01-01 19:23:18 +0100

    Added exit menu entry

--- SimpleNotepad/MainWindow.h
+++ SimpleNotepad/MainWindow.h
@@ -5,20 +5,24 @@ class MainWindow
 {
 public:
 	HWND hEditBox;
-	enum RESOURCE { FILE_NEW, FILE_OPEN, FILE_SAVE, FILE_SAVE_AS};
+	enum RESOURCE { FILE_NEW = 1, FILE_OPEN, FILE_SAVE, FILE_SAVE_AS, FILE_EXIT};
 
 	MainWindow(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow, LRESULT(*WindowProcedure)(HWND, UINT, WPARAM, LPARAM));
 	~MainWindow();
 
 	void start();
 	void addComponents(HWND hWnd, HWND& hEditB);
+	void save(HWND hWnd);
 	void getSaveFilePath(HWND hWnd);
 	void getOpenFilePath(HWND hWnd);
+	void exit(HWND hWnd);
 
 private:
 	WNDCLASSW wc = {};
 	HINSTANCE hInstance, hPrevInstance;
 	LPSTR lpCmdLine;
+	bool saveChanged = false;
+	char path[10000];
 	int nCmdShow;
 	HWND hMainWindow;
 	LRESULT (*WindowProcedure)(HWND, UINT, WPARAM, LPARAM);
